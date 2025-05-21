7906cda5549ef73e25a12ba11dae9859e8814cfc
compost-em-all <jtaylor9110@gmail.com>
2025-05-21 00:18:47 -0400

    move file picking logic out to filepicker component

--- shareToFile/app/(tabs)/index.tsx
+++ shareToFile/app/(tabs)/index.tsx
@@ -1,37 +1,12 @@
-import * as DocumentPicker from 'expo-document-picker';
-import { useState } from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
-// import FilePicker from "../components/filepicker";
-
-const [selectedDocument, setSelectedDocument] = useState<DocumentPicker.DocumentPickerAsset>();
-
-const pickDocument = async () => {
-    try {
-        const result = await DocumentPicker.getDocumentAsync({ copyToCacheDirectory: true});
-
-        if (!result.canceled) {
-            const successResult = result as DocumentPicker.DocumentPickerSuccessResult;
-
-            setSelectedDocument(result.assets[0]);
-
-        } else {
-            console.log("Document selection cancelled");
-        }
-    } catch (error) {
-        console.log("Picking document:", error);
-    }
-}
-
-
+import FilePicker from "../components/filepicker";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
-      {/* <FilePicker /> */}
-      <TouchableOpacity onPress={() => pickDocument()}></TouchableOpacity>
-      
+      <FilePicker />
     </View>
   );
 }
--- shareToFile/app/components/filepicker.tsx
+++ shareToFile/app/components/filepicker.tsx
@@ -1,29 +1,43 @@
 import * as DocumentPicker from 'expo-document-picker';
 import { useState } from "react";
-import { Button } from "react-native";
-
-const [selectedDocument, setSelectedDocument] = useState<DocumentPicker.DocumentPickerAsset>();
-
-const pickDocument = async () => {
-    try {
-        const result = await DocumentPicker.getDocumentAsync();
-
-        if (!result.canceled) {
-            const successResult = result as DocumentPicker.DocumentPickerSuccessResult;
-
-            setSelectedDocument(result.assets[0]);
-
-            alert(selectedDocument?.name);
-        } else {
-            console.log("Document selection cancelled");
-        }
-    } catch (error) {
-        console.log("Foo picking document:", error);
-    }
-}
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
 export default function FilePicker() {
+    const [selectedDocument, setSelectedDocument] = useState<DocumentPicker.DocumentPickerAsset>();
+
+    const pickDocument = async () => {
+        try {
+            const result = await DocumentPicker.getDocumentAsync({ copyToCacheDirectory: true});
+    
+            if (!result.canceled) {
+                const successResult = result as DocumentPicker.DocumentPickerSuccessResult;
+                console.log(result);
+                console.log(successResult);
+    
+                setSelectedDocument(result.assets[0]);
+    
+            } else {
+                console.log("Document selection cancelled");
+            }
+        } catch (error) {
+            console.log("Picking document:", error);
+        }
+    }
+
     return (
-        <Button title="Select file" onPress={pickDocument}></Button>
+        <TouchableOpacity onPress={() => pickDocument()}>
+            <Text style={styles.button}>Pick a Document</Text>
+        </TouchableOpacity>
     );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#fff'
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+})
